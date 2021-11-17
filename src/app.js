2fd44eb329fa7e276ae9b8fc03602d2ef8015f8b
Ubuntu <ubuntu@ip-172-31-30-34.us-east-2.compute.internal>
2021-11-17 22:52:04 +0000

    Tesst

--- src/app.js
+++ src/app.js
@@ -1,68 +1,65 @@
-const express = require('express')
-const morgan = require('morgan')
-const exphbs = require('express-handlebars')
-const path = require('path')
-const session = require('express-session')
+const express = require('express');
+const morgan = require('morgan');
+const pkg = require('../package.json');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const session = require('express-session');
 const MySQLStore = require('express-mysql-session')
-const passport = require('passport')
-const cors = require('cors')
+const passport = require('passport');
+var cors = require('cors')
 require('dotenv').config()
 
-// INIT
-const app = express()
-require('./libs/passport')
-app.use(cors())
 
-// SETTINGS
-app.set('views', path.join(__dirname, 'views'))
-app.engine(
-  '.hbs',
-  exphbs({
+//INIT
+const app = express();
+require('./libs/passport');
+app.use(cors());
+
+//SETTINGS
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs'
-  })
-)
-app.set('view engine', '.hbs')
+    extname: '.hbs',
+}));
+app.set('view engine', '.hbs');
 
-// MIDDLEWARES
-app.use(
-  session({
+//MIDDLEWARES
+app.use(session({
     secret: process.env.SECRET_PASSPORT,
     resave: false,
     saveUninitialized: false,
     store: new MySQLStore({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
     })
-  })
-)
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.disable('etag')
+}))
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.disable('etag');
 
-// GLOBAL VARIABLES
+//GLOBAL VARIABLES
 app.use((req, res, next) => {
-  app.locals.user = req.user
-  next()
-})
+    app.locals.user = req.user
+    next();
+});
 
-// ROUTES
-app.use('/blog', require('./routes/blog'))
-app.use('/category', require('./routes/category'))
-app.use('/user', require('./routes/user'))
-app.use('/project', require('./routes/projects'))
+//ROUTES
+app.use('/blog', require('./routes/blog'));
+app.use('/category', require('./routes/category'));
+app.use('/user', require('./routes/user'));
+app.use('/project', require('./routes/projects'));
 app.get('/', (req, res) => {
-  res.redirect('/user')
-})
+    res.redirect('/user')
+});
 
-// PUBLIC
-app.use(express.static(__dirname.concat('/public')))
+//PUBLIC
+app.use(express.static(__dirname + '/public'));
 
-module.exports = app
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-const app = require('./app')
+const app = require('./app');
 require('dotenv').config()
 
-// SETTING
-app.set('port', process.env.PORT || 80)
+//SETTING
+app.set('port', process.env.PORT || 3000);
 
-// LISTEN
-app.listen(app.get('port'))
+//LISTEN
+app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
+});
