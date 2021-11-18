13f2127544a84955124a6977814e1041e138b728
Ubuntu <ubuntu@ip-172-31-30-34.us-east-2.compute.internal>
2021-11-18 19:37:51 +0000

    merge

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-const app = require('./app');
-require('dotenv').config()
-
-//SETTING
-app.set('port', process.env.PORT || 3000);
-
-//LISTEN
-app.listen(app.get('port'), () => {
-    console.log('server on port', app.get('port'));
-});
-=======
 const app = require('./app')
 require('dotenv').config()
 
@@ -18,4 +6,3 @@ app.set('port', process.env.PORT || 3000)
 
 // LISTEN
 app.listen(app.get('port'))
->>>>>>> 2e3b59f7ca0af699c74f08e17a7b67313bef80e0
