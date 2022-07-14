b7f8fddb834c4677281c9e1c72e8819413f2d7ce
ale_figure8 <gomeznieto@gmail.com>
2022-07-14 18:39:01 -0300

    f**k with this bug

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -93,7 +93,7 @@ export const postUpdate = async (req, res) => {
       category
     }
 
-    if (req.file) {
+    if (req.file !== undefined) {
       newPost.img = `/img/${req.file.filename}`
     }
 
