f8642913b69c90606c971b1f544a712faf866c33
ale_figure8 <gomeznieto@gmail.com>
2022-07-22 10:21:13 -0300

    change cert

--- src/index.js
+++ src/index.js
@@ -1,32 +1,25 @@
 import {app} from './app.js'
 import https from 'https'
-import http from 'http'
+import path from 'path'
 import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
 // CERTIFICATE
 const options = {
-  key: fs.readFileSync('./src/certs/privatekey.pem'),
-  cert: fs.readFileSync('./src/certs/server.crt')
+  key: fs.readFileSync(path.join('./src/certs/server-key.pem')),
+  cert: fs.readFileSync(path.join('./src/certs/server-cert.pem'))
 }
 
 // SETTING
-const port = 443 || 8080
+const port = 8080
 
 // LISTENING
 const server = https.createServer(options, app).listen(port, () => {
   console.log('Server listening on port ' + port)
 })
 
-const server2 = http.createServer(app).listen(8080, () => {
-  console.log('Server listening on port ' + 8080)
-})
-
 server.on('error', err => {
   console.log(err)
 })
 
-server2.on('error', err => {
-  console.log(err)
-})
