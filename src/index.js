81cbdf08d085b6b4dbdf3a451ff8388de7a6d979
ale_figure8 <gomeznieto@gmail.com>
2022-07-22 09:50:53 -0300

    change cert and port

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import {app} from './app.js'
+import http from 'http'
+import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
+// CERTIFICATE
+const options = {
+  key: fs.readFileSync('./src/certs/privatekey.pem'),
+  cert: fs.readFileSync('./src/certs/server.crt')
+}
+
 // SETTING
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 443)
 
 // LISTENING
-const server = app.listen(app.get('port') || 80, () => {
-    console.log('Server listening on port ' + app.get('port'))
+const server = http.createServer(options, app).listen(app.get('port'), () => {
+  console.log('Server listening on port ' + app.get('port'))
 })
 
 server.on('error', err => {
