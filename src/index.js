f91ebec4ec06de180c039e781f25a5da068d7248
ale_figure8 <gomeznieto@gmail.com>
2022-07-22 09:38:53 -0300

    rolback

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import {app} from './app.js'
-import http from 'http'
-import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
-
-// CERTIFICATE
-const options = {
-  key: fs.readFileSync('./src/certs/server-key.pem'),
-  cert: fs.readFileSync('./src/certs/server-cert.pem')
-}
 
 // SETTING
 app.set('port', process.env.PORT || 3000)
 
 // LISTENING
-const server = http
-  .createServer(options, app)
-  .listen(app.get('port') || 80, () => {
+const server = app.listen(app.get('port') || 80, () => {
     console.log('Server listening on port ' + app.get('port'))
-  })
+})
 
 server.on('error', err => {
   console.log(err)
