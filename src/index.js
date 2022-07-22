2c767640e0d87bf2716cf2e82951af12d3de2a2f
ale_figure8 <gomeznieto@gmail.com>
2022-07-22 08:44:32 -0300

    certificate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import {app} from './app.js'
+import http from 'http'
+import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
+
+// CERTIFICATE
+const options = {
+  key: fs.readFileSync('./certs/server-key.pem'),
+  cert: fs.readFileSync('./certs/server-cert.pem')
+}
 
 // SETTING
 app.set('port', process.env.PORT || 80)
 
-// LISTEN
-app.listen(app.get('port'))
+// LISTENING
+const server = http.createServer(options, app).listen(app.get('port') || 80)
+
+server.on('error', err => {
+  console.log(err)
+})
