0f07cb57d5c9b1fff11972aab18ed6a81a0e7626
ale_figure8 <gomeznieto@gmail.com>
2022-07-22 17:11:57 -0300

    Eraser certs

--- src/index.js
+++ src/index.js
@@ -1,32 +1,13 @@
 import {app} from './app.js'
-import https from 'https'
 import http from 'http'
-import path from 'path'
-import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-// CERTIFICATE
-const options = {
-  key: fs.readFileSync(path.join('./src/certs/server-key.pem')),
-  cert: fs.readFileSync(path.join('./src/certs/server-cert.pem'))
-  //ca: fs.readFileSync(path.join('./src/certs/ca.pem'))
-}
-
 // SETTING
 const port = process.env.PORT || 3000
-const port2 = process.env.PORT2 || 3001
 
-// LISTENING
-const httpsServer = https.createServer(options, app).listen(port, () => {
+const httpServer = http.createServer(app).listen(port, () => {
   console.log('Server listening on port ' + port)
-})
-const httpServer = http.createServer(app).listen(port2, () => {
-  console.log('Server listening on port ' + port2)
-})
-
-httpsServer.on('error', err => {
-  console.log(err)
 })
 
 httpServer.on('error', err => {
