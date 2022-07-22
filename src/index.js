c6c52374a7e10c000ab3164b827f2ee0dbbdee6b
ale_figure8 <gomeznieto@gmail.com>
2022-07-22 10:10:35 -0300

    second rules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {app} from './app.js'
 import https from 'https'
+import http from 'http'
 import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
@@ -18,6 +19,14 @@ const server = https.createServer(options, app).listen(port, () => {
   console.log('Server listening on port ' + port)
 })
 
+const server2 = http.createServer(app).listen(8080, () => {
+  console.log('Server listening on port ' + 8080)
+})
+
 server.on('error', err => {
+  console.log(err)
+})
+
+server2.on('error', err => {
   console.log(err)
 })
