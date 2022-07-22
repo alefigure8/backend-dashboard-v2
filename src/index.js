49b742774dc9839ef36140cce71ea552e2d8db54
ale_figure8 <gomeznieto@gmail.com>
2022-07-22 10:46:58 -0300

    certs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import {app} from './app.js'
+import https from 'https'
+import path from 'path'
+import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
+
+// CERTIFICATE
+const options = {
+  key: fs.readFileSync(path.join('./src/certs/server-key.pem')),
+  cert: fs.readFileSync(path.join('./src/certs/server-cert.pem'))
+}
 
 // SETTING
 const port = process.env.PORT || 3000
 
 // LISTENING
-const server = app.listen(port, () => {
+const server = https.createServer(options, app).listen(port, () => {
   console.log('Server listening on port ' + port)
 })
 
